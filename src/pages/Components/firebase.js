4ea6ed612850f55e3bc73b5109f666c5189ae4e7
Aunish_2509 <145715074+avy252004@users.noreply.github.com>
2025-07-24 21:57:40 +0530

    Update firebase.js

--- src/pages/Components/firebase.js
+++ src/pages/Components/firebase.js
@@ -5,14 +5,7 @@ import { getFirestore } from "firebase/firestore"; // For Firestore (choose one)
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAOmYuWwTpu_D4bQafjlAi-4MXV0ME1Fks",
-  authDomain: "wds09-e6d91.firebaseapp.com",
-  databaseURL: "https://wds09-e6d91-default-rtdb.firebaseio.com",
-  projectId: "wds09-e6d91",
-  storageBucket: "wds09-e6d91.firebasestorage.app",
-  messagingSenderId: "626885264348",
-  appId: "1:626885264348:web:de2a3e0a4622d0589a4519",
-  measurementId: "G-409VWSGFSH"
+  ur config
 };
 
 const app = initializeApp(firebaseConfig);
